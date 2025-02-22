718ee938a1637d87e61bda67e9d59d673e819179
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-02-22 10:23:18 +0000

    initial commit

---
+++ welcometotheworldofcoding.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("welcome to the world of coding")
+
+#print a number
+print("4")
+
+#next line after print
+print("Hello python \n")
+
+#pass multiple arguements for printing
+print("hello,5")
+
+#end  arguements of print statments
+print("welcome to", end= "*")
