fa3fe9587bce2d18ce15f5f2dded4b6069224b3f
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-02-23 20:23:03 +0000

    initial commit

---
+++ storingbirthdays.py
@@ -0,0 +1,17 @@
+#storing birthdays
+
+#first birthday
+x= 19-2
+print(x)
+#second birthday
+y= 22-1
+print(y)
+#third birthday
+a= 6-3
+print(a)
+#fourth birthday
+b= 9-4
+print(b)
+#fifth birthday
+c= 15-3
+print(c)
