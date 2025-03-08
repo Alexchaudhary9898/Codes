495a6f33d5608219bbc43f863cb2ab83d650e994
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-03-08 10:27:38 +0000

    initial commit

---
+++ Andoroperator.py
@@ -0,0 +1,20 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast one number has boolean value as false")
+
+a = 10
+b = -10
+c = 0
+if a > 0 or b > a:
+    print("either of the number is greater than 0")
+else:
+    print("no number is greather then 0")
+
+if b > 0 or c > 0:
+    print("either of the number is greather then 0")
+else:
+    print("no number is greather than 0")
