f00022a612dc383ac469c96bc881ea3d8c74371b
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-03-15 10:54:15 +0000

    initial commit

---
+++ powercalculator.py
@@ -0,0 +1,7 @@
+number = float(input("enter the base number:"))
+
+exponent = int(input("enter the exponent (n):"))
+
+result = number ** exponent
+
+print(f"the result of {number} raised to power of {exponent} is result:")
