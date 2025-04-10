1a1c2dd55d1e918ad699cb5b6515a6060a309e33
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-04-10 22:00:42 +0100

    initial commit

--- dueamount.py
+++ dueamount.py
@@ -3,7 +3,7 @@ amount_paid = float(input("enter the amount paid"))
 if amount_paid < bill_amount:
     due_amount = bill_amount - amount_paid
     print(f"customer still owes")
-elif amount_paid > bill_amount
+elif amount_paid > bill_amount:
     change = amount_paid - bill_amount
     print(f"customer has overpaid. return {change:.2f}")
 else:
