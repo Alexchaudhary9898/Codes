464603d5c927fc1384be1bd0c78b0b4a851586f4
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-06-15 11:45:55 +0100

    initial commit

---
+++ imageexample.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((500,400))
+pygame.display.set_caption("image example")
+image = pygame.image.load("download.jpg")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255)) # Optional background color
+    screen.blit(image, (100, 100)) # Display the image on screen
+    pygame.display.update()
+pygame.quit()
