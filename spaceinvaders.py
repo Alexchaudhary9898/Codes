3a8e4d3deb08ba1ad1437b3d2d3d53fa9316d5ed
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-07-05 11:46:02 +0100

    initial commit

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -4,7 +4,7 @@ pygame.init()
 WIDTH, HEIGHT = 600, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("space invader with sprites")
-clock = pygame.tick.Clock()
+clock = pygame.time.Clock()
 player_img = pygame.image.load("OIP.webp")
 enemy_img = pygame.image.load("download 2.webp")
 bullet_img = pygame.image.load("download.webp")
@@ -15,9 +15,30 @@ player_rect = player_img.get_rect(midbottom=(300, 300))
 enemy_rect = enemy_img.get_rect(topleft=(random.randint(0, 500), 50))
 bullet_rect = bullet_img.get_rect(center=(-100, -100))
 running = True
+bullet_active = False
 while running:
     clock.tick(60)
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_rect.left > 0:
+        player_rect.x -= 5
+    if keys[pygame.K_RIGHT] and player_rect.right < WIDTH:
+        player_rect.x += 5
+    if keys[pygame.K_SPACE] and not bullet_active:
+        bullet_rect.center = player_rect.midtop
+        bullet_active = True
+    if bullet_active:
+        bullet_rect.y -= 7
+        if bullet_rect.bottom < 0:
+            bullet_active = False
+        screen.blit(bullet_img, bullet_rect)
+    if bullet_rect.colliderect(enemy_rect):
+        enemy_rect.x = random.randint(0, 500)
+        bullet_active = False
+    screen.blit(player_img,player_rect)
+    screen.blit(enemy_img, enemy_rect)
+    pygame.display.flip()
+pygame.quit()
