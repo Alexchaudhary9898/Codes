5cf2e94259f3f1b8a256f0a22b1a5bcb8880f0d9
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-07-06 11:52:25 +0100

    initial commit

---
+++ gettingstartedwithwidgets.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from datetime import date
+root = Tk()
+root.title('getting started with widgets')
+root.geometry('400x300')
+lbl = Label(text="hey there!",fg="white", bg="#072f5f", height=1, width=300)
+name_entry = Entry()
+def display():
+    name = name_entry.get()
+    global Message
+    message = "welcome to the applications \n today's date is:"
+    greet = "hello "+name+"\n"
+    text_box.insert(END, greet)
+    text_box.insert(END, message)
+    text_box.insert(END, date.today())
+text_box = Text(height=3)
+btn = Button(text="begin", command=display, height=1, bg="#126140", fg='white')
+lbl.pack()
+name_entry.pack()
+text_box.pack()
+root.mainloop()
