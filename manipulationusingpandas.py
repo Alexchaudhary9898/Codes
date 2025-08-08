72f69f593d9cd1db8356d43f18bc0e323e69fb5f
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-08-08 21:29:39 +0100

    initial commit

--- manipulationusingpandas.py
+++ manipulationusingpandas.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
 info_data = {'Name': ['john', 'scarlett', 'anna', 'emma', 'johnny', 'ash'],
-        'age': ['10', '9', np.nan '12', '11', '13', '14', np.nan '8'],
-        'grade': ['5', '4', '6', '6', '7', '8', '4']}
+        'age': ['10', '9','12', '11', '13', '14','8'],
+        'grade': ['5', '4', '6', '6', '7', '8', '4'],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(info_data , index=labels)
+print("Summary of the basic information about this Dataframe and its data:")
+print(df.info())
