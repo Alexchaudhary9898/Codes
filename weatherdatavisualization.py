61d13bd34704200520866ac2a9efaafec2e26716
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-08-16 11:38:51 +0100

    initial commit

---
+++ weatherdatavisualization.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+weather = pd.read_csv("Test.csv")
+print(weather.head())
+weather.info()
+sns.barplot(x=weather['humidity'], y=weather['temperature'])
+plt.show()
+weather.info()
+sns.displot(weather['humidity'])
+plt.show()
+sns.displot(weather['humidity'], kde=False, rug=True)
+plt.show()
+print(weather.head())
+weather.info()
+sns.jointplot(x='humidity', y='temperature', data=weather)
+plt.show()
+sns.jointplot(x='humidity', y='temperature', data=weather, kind="hex")
+plt.show()
+sns.jointplot(x='humidity', y='temperature', data=weather, kind="kde")
+plt.show()
