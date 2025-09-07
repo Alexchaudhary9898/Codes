7df4cc45185f8ec1e98c7b9eb7e42328af520e80
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2025-09-07 11:03:39 +0100

    initial commit

---
+++ enhancedaichatbot.py
@@ -0,0 +1,16 @@
+print("hello i am enhanced ai chat bot. whats your name")
+name = input()
+print(f"nice to meet you {name}")
+print("how are you feeling today (good/bad) :")
+mood = input().lower()
+if mood == "good":
+    print("im happy to hear that")
+elif mood == "bad":
+    print("im sorry to hear that")
+else: 
+    print("i see sometimes its hard to put feelings into words")
+print("what are your hobbies")
+hobbies = input()
+print("what grade are you in")
+grade = input()
+print("thats good to hear")
