7cfe4fecc8fc92a076e1ab46d711e62520b1f2b2
Alexchaudhary9898 <chaudharyalex2019@gmail.com>
2026-02-15 09:48:12 +0000

    Add files via upload

--- aiteachingassistant.py
+++ aiteachingassistant.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import cohere
 import config
-import re   
+import re 
+import io  
 co = cohere.Client(config.COHERE_API_KEY)
 def ai(prompt, tokens = 800):
     try:
@@ -33,3 +34,46 @@ if question:
     st.markdown(answer)
 else:
     st.info("Please enter a question")
+
+def export_chat(history):
+    text = ""
+    for i, item in enumerate(history, 1):
+        text += f"Q{i}: {item['question']}\n"
+        text += f"A{i}: {item['answer']}\n\n"
+    return io.BytesIO(text.encode("utf-8"))
+if "history" not in st.session_state:
+    st.session_state["history"] = []
+st.set_page_config(page_title="Ai teaching assistant", layout="centered")
+st.title("AI teaching Assistant (Cohere)")
+st.write("Ask any question and get a complete structured answer")
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Clear chat"):
+            st.session_state.history = []
+            st.rerun()
+with col2:
+    if st.session_state.history:
+        st.download_button(
+         "Export chat",
+            data=export_chat(st.session_state.history),
+            file_name="AI_CHAT_HISTORY.txt",
+            mine="Text/plain"   
+        )
+question = st.text_input("Enter your question:", key="question_input")
+if st.button("Ask"):
+    q = question.strip()
+    if q:
+        with st.spinner("Generating complete answer"):
+            answer = complete_answer(q)
+        st.session_state.history.insert(0, {
+            "question": q,
+            "answer": answer
+        })
+        st.rerun()
+    else:
+        st.warning("Please enter a question")
+st.markdown("Conversation history")
+for i, item in enumerate(st.session_state.history, 1):
+    st.markdown (f"**Q{i}: {item['question']}**")
+    st.markdown(item['answer'])
+    st.markdown("---")
